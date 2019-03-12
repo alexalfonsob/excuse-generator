8a47285714e86f906e6394e4739bbb0fb207316e
alexalfonsob <48039568+alexalfonsob@users.noreply.github.com>
2019-03-12 13:44:58 -0400

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,12 @@
+function excuse() {
+    let who = ['the dog','my granma','his turtle','my bird'];
+    let what = ['eat','pissed','crushed','broked'];
+    let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+    
+    let Who = who[Math.round(Math.random()*(who.length-1))];
+    let When = when[Math.round(Math.random()*(when.length-1))];
+    let What = what[Math.round(Math.random()*(what.length-1))];
+
+    document.getElementById('excuse').innerHTML = '<div>' + Who + ' ' + When + ' '  + What + '.</div>'
+
+}
